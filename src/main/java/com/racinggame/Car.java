d7c8525dd84722b46a5b6a0b793cb0d707f60323
Ursu Tatiana <russu.tatiana90@gmail.com>
2020-11-03 16:15:34 +0300

    added Car class; studied variables

---
+++ src/main/java/com/racinggame/Car.java
@@ -0,0 +1,14 @@
+package com.racinggame;
+
+public class Car {
+    String name;
+    double fuelLevel;
+    double mileage;
+    double totalTraveledDistance;
+    double maxSpeed;
+
+    int doorCount;
+    boolean damaged;
+    String color;
+
+}
