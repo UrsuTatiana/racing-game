fe5b53e24b7713efb5598496882de692b034ec29
Ursu Tatiana <russu.tatiana90@gmail.com>
2020-11-05 13:40:35 +0300

    studied inheritance

--- src/main/java/com/racinggame/Vehicle.java
+++ src/main/java/com/racinggame/Vehicle.java
@@ -1,6 +1,8 @@
 package com.racinggame;
 
 public class Vehicle {
+
+    //instance variables
     String name;
     double fuelLevel;
     double mileage;
@@ -9,4 +11,27 @@ public class Vehicle {
 
     boolean damaged;
     String color;
+
+    public double accelerate(double speed, double durationInHours){
+
+        //Ferrari is accelerating with 100km/h for 1h
+        System.out.println(name + "is accelerating with" + speed + "km/h for" + durationInHours + "h");
+
+        //local variable
+        double distance = speed * durationInHours;
+
+        totalTraveledDistance = totalTraveledDistance + distance;
+        // sau  totalTraveledDistance +=  distance;
+        System.out.println("Total traveled distance for vehicle" + name + ": " + totalTraveledDistance);
+
+
+        double usedFuel = distance * mileage/100;
+
+        fuelLevel = fuelLevel - usedFuel;
+       // fuelLevel -= usedFuel
+        System.out.println("Remaining fuel for vehicle" + name + ": " + fuelLevel);
+
+
+        return distance;
+    }
 }
