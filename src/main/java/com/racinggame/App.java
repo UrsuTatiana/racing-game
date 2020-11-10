1671e2d62a5cbe74ee776e3e8a41c92a02b01097
Ursu Tatiana <russu.tatiana90@gmail.com>
2020-11-10 13:06:28 +0300

    studied conditional statements

--- src/main/java/com/racinggame/App.java
+++ src/main/java/com/racinggame/App.java
@@ -9,7 +9,7 @@ public class App {
        carReference.fuelLevel = 60;
        carReference.mileage = 12.5;
        carReference.totalTraveledDistance = 0;
-       carReference.damaged = true;
+       carReference.damaged = false;
        carReference.doorCount = 1;
        carReference.color = "Red";
 
@@ -42,6 +42,8 @@ public class App {
         car2.name = "Renault";
         car2.fuelLevel = 70;
         car2.totalTraveledDistance = 100;
+        car2.maxSpeed = 300;
+        car2.mileage = 10;
 
         car2.engine = new Engine();
         car2.engine.capacity = 2000;
@@ -55,9 +57,11 @@ public class App {
         System.out.println("Door count: " + car2.doorCount);
         System.out.println("Colour: " + car2.color);
 
+        System.out.println("*************");
+
         double distanceForCar1 = carReference.accelerate(60,1);
 
-        double distanceForCar2 = car2.accelerate(100,1);
+        double distanceForCar2 = car2.accelerate(130,1);
 
         System.out.println("Engine manufacturer name before repair: " + carReference.engine.manufacturer );
         Mechanic mechanic = new Mechanic();
--- src/main/java/com/racinggame/Mechanic.java
+++ src/main/java/com/racinggame/Mechanic.java
@@ -1,11 +1,13 @@
 package com.racinggame;
 
 public class Mechanic {
+
     public void repair(AutoVehicle autoVehicle){
 
         autoVehicle.engine = new Engine();
 
         autoVehicle.damaged = false;
+
 
     }
 }
